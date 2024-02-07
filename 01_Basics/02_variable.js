3a6add55bdd8a6e97cd1b6f7f51bfbea617c9780
NakulKait <129101347+NakulKait@users.noreply.github.com>
2024-02-07 19:33:46 +0000

    ok

---
+++ 01_Basics/02_variable.js
@@ -0,0 +1,12 @@
+const accountId = 12456
+let accountEmail = "nakulkait@gmail.com"
+var accountPassword = "12345"
+accountCity = "Pune"
+let accountState
+accountEmail= "nakulkait2002@gmail.com"
+accountPassword = "1215233"
+accountCity = "Pusad"
+
+console.log(accountId)
+
+console.table([accountId , accountEmail ,accountPassword , accountCity ,accountState  ])
---
+++ 01_Basics/03_typeconversion.js
@@ -0,0 +1,38 @@
+//string to number
+
+/* let score  = "33"
+console.log(typeof score)
+let New_number = Number(score)
+console.log(typeof New_number)
+
+let score1 = "33abc"
+console.log(typeof score1)
+
+let New_number1 = Number(score1)
+console.log(typeof New_number1)
+console.log(New_number1)
+
+*/
+//Boolean
+
+let isLoggedIn = 1
+console.log(typeof isLoggedIn)
+let boolean_isLoggedIn = Boolean(isLoggedIn)
+console.log(typeof boolean_isLoggedIn)
+console.log(boolean_isLoggedIn)
+
+let name = ""
+let boolean_name = Boolean(name)
+console.log(boolean_name)
+
+let name1 = "N"
+let boolean_name1 = Boolean(name1)
+console.log(boolean_name1)
+
+let somenum = 33
+let string_somenum = String(somenum)
+console.log(typeof string_somenum)
+
+let somenum1 = true
+let string_somenum1 = String(somenum1)
+console.log(string_somenum1)
