dabc66647415363b172e0835c2c9f8a5f98d6869
NakulKait <129101347+NakulKait@users.noreply.github.com>
2024-02-10 06:28:29 +0000

    ok

---
+++ 02_Basics/01_Arrays.js
@@ -0,0 +1,19 @@
+let myArr = [2,6,2,4,8,9]
+// let myArr2 = ["Nakul",  "Joke" , "Set"]
+// let myArr3 = new Array(1,6,96)
+
+// console.log(myArr[0])
+
+// //Methods Of Array
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+
+let newArr1 = myArr.slice(1,3)
+console.log(newArr1)
+console.log(myArr)
+let newArr2 = myArr.splice(1,3)
+console.log(newArr2)
+console.log(myArr)
