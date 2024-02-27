028b0733ef221085c5de132f2505e1d2332e402d
NakulKait <129101347+NakulKait@users.noreply.github.com>
2024-02-27 08:58:47 +0000

    done

---
+++ 05_Iterations/01_For_Loop.js
@@ -0,0 +1,18 @@
+for(let i = 0;i<=10;i++){
+    console.log(i)
+}
+
+for(let i =0;i<=10;i++){
+    if (i==5) {
+        console.log("5 is detected ")
+        break
+    }
+    console.log(i)
+}
+for(let i =0;i<=10;i++){
+    if (i==5) {
+        console.log("5 is detected ")
+        continue
+    }
+    console.log(i)
+}
