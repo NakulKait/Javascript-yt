bfb4f14ea5ed83d091c4ec48bdd563fa1745eff6
NakulKait <129101347+NakulKait@users.noreply.github.com>
2024-02-28 16:46:45 +0000

    done

--- 05_Iterations/05_Map_Reduce.js
+++ 05_Iterations/05_Map_Reduce.js
@@ -23,4 +23,16 @@ const val4 = [1,2,3,4,5,6,7,8,9]
 const val5 = val4.map((num)=>num+10)
 console.log( val5 )
 
+//chaining 
+const newArr = val4.map((num)=>num*10).map((num)=>num+1).filter((num)=>num>40)
+console.log(newArr)
 
+//Reduce
+// There are two parameters in call back function which is accumulator and current value 
+// for accumulator there is intial value which is given at the end of function
+
+const newArr2 = val4.reduce((acc , currval)=>{
+    console.log(`The value of accumulator is ${acc} and current value is ${currval}`)
+    return acc + currval
+} , 0 )
+console.log(newArr2);
